6e6c66bba5e229775a2e472bc4b021ac962c50e3
Hasegawa Yohei <yoheybrains@gmail.com>
2018-11-21 00:01:35 +0900

    modified: add createValidationQueries fucntion

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 (function () {
   'use strict';
 
+  function createValidationQueries (inputs) {
+    return Array.from(inputs).map(input => ({
+      name: input.name,
+      type: input.dataset.validation,
+      value: input.value
+    }));
+  }
+
   function validateForm(form) {
     const result = {
       get isValid() {
@@ -12,18 +20,18 @@
 
     const inputs = Array.from(form.querySelectorAll('input'));
 
-    for (let input of inputs) {
-      if (input.dataset.validation === 'alphabetical') {
-        let isValid = /^[a-z]+$/i.test(input.value);
+    for (let validation of createValidationQueries(form.querySelectorAll('input'))) {
+      if (validation.type === 'alphabetical') {
+        let isValid = /^[a-z]+$/i.test(validation.value);
 
         if (!isValid) {
-          result.errors.push(new Error(`${input.value} is not a valid ${input.name} value`));
+          result.errors.push(new Error(`${validation.value} is not a valid ${validation.name} value`));
         }
-      } else if (input.dataset.validation === 'numeric') {
-        let isValid = /^[0-9]+$/.test(input.value);
+      } else if (validation.type === 'numeric') {
+        let isValid = /^[0-9]+$/.test(validation.value);
 
         if (!isValid) {
-          result.errors.push(new Error(`${input.value} is not a valid ${input.name} value`));
+          result.errors.push(new Error(`${validation.value} is not a valid ${validation.name} value`));
         }
       }
     }
@@ -95,6 +103,30 @@
       expect(result.errors[0].message).to.equal('!!! is not a valid first-name value');
       expect(result.errors[1]).to.be.instanceof(Error);
       expect(result.errors[1].message).to.equal('a is not a valid age value');
+    });
+  });
+
+  describe('th createValidationQueries function', () => {
+    let form = document.querySelector('.test-form');
+
+    it('should map input elements with a data-validation attribute to an array of validation objects', () => {
+      const name = form.querySelector('input[name="first-name"]');
+      const age  = form.querySelector('input[name="age"]');
+
+      name.value = 'Bob';
+      age.value  = '42';
+
+      const validations = createValidationQueries([name, age]);
+
+      expect(validations.length).to.equal(2);
+
+      expect(validations[0].name).to.equal('first-name');
+      expect(validations[0].type).to.equal('alphabetical');
+      expect(validations[0].value).to.equal('Bob');
+
+      expect(validations[1].name).to.equal('age');
+      expect(validations[1].type).to.equal('numeric');
+      expect(validations[1].value).to.equal('42');
     });
   });
 
